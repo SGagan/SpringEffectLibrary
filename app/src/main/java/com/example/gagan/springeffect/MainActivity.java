e80abe86514460e4b8ad202672d38bc9c0f1ef4b
GaganDeep Singh <sgagan789@gmail.com>
2019-01-13 20:21:54 +0530

    Logic Done

--- app/src/main/java/com/example/gagan/springeffect/MainActivity.java
+++ app/src/main/java/com/example/gagan/springeffect/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.gagan.springeffect;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.gagan.springeffectlibrary.SpringEffectListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SpringEffectListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=(SpringEffectListView)findViewById(R.id.listView);
+        View topView=View.inflate(this,R.layout.top,null);
+        AbsListView.LayoutParams params = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, 500);
+        topView.setLayoutParams(params);
+        listView.addHeaderView(topView);
+        listView.setAdapter(new DemoAdapter());
+    }
+    class DemoAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 20;
+        }
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+        @SuppressLint("DefaultLocale")
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv;
+            if(convertView instanceof TextView){
+                tv= (TextView) convertView;
+            }else{
+                tv=new TextView(getApplicationContext());
+                tv.setTextColor(Color.BLACK);
+                tv.setBackgroundColor(Color.WHITE);
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP,20);
+            }
+            tv.setText(String.format("position%d", position));
+            return tv;
+        }
+
     }
 }
